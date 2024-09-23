ee3a3933a3fad210aa7467503f0a219a857ded41
J0sueTM <teodoro.josue@protonmail.ch>
2024-09-23 15:53:48 -0300

    feat: diff prod and dev on webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-const path = require('path'); // Use Node.js 'path' module
+const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  context: path.resolve(__dirname, 'target'), 
-  entry: './mockingbird.lib.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/, // Match CSS files
-        use: ['style-loader', 'css-loader'], 
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', 
-  },
+module.exports = (env) => {
+  const isProduction = env.isProduction === 'true';
+  
+  return {
+    mode: (isProduction ? 'production' : 'development'),
+    context: path.resolve(__dirname, 'target'), 
+    entry: './mockingbird.lib.js',
+    module: {
+      rules: [
+        {
+          test: /\.js$/, 
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'], 
+        },
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'mockingbird-lib' +
+        (isProduction ? '.min' : '') +
+        '.js',
+    },
+  };
 };
-;
